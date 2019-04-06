a543e7c4e4ec8919ad1a66e02a0a77470d4ab3b3
AntiqueR <antique.pendulum@gmail.com>
2019-04-06 15:33:35 +0900

    Hello Worldした

---
+++ PrismLearn/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Mvvm;
+
+namespace PrismLearn.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string _text = "Hello World";
+        public string Text
+        {
+            get { return _text; }
+            set { SetProperty(ref _text, value); }
+        }
+    }
+}
